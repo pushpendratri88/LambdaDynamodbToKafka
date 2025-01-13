792b85e0ac758c72fb19a7920973842f421edb1b
Pushpendra.tripathi <pushpendra.tri88@gmail.com>
2025-01-13 16:12:35 +0530

    aws lambda working code with kafka configuration

--- src/main/java/com/brainstorm/handler/DynamoDBStreamHandler.java
+++ src/main/java/com/brainstorm/handler/DynamoDBStreamHandler.java
@@ -4,18 +4,20 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.apache.kafka.clients.producer.KafkaProducer;
-//import org.apache.kafka.clients.producer.ProducerConfig;
-//import org.apache.kafka.clients.producer.ProducerRecord;
-//import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 
-//import java.io.IOException;
-//import java.net.Socket;
-//import java.util.Properties;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Properties;
+
 
 public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, String> {
 
@@ -24,35 +26,31 @@ public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, Stri
     private static final String TOPIC = "raw-events";
     private static final String BOOTSTRAP_SERVERS = "13.203.157.163:9092";
 
-//    private static KafkaProducer<String, String> kafkaProducer;
+    private static KafkaProducer<String, String> kafkaProducer;
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     // Static block to initialize Kafka Producer once
-//    static {
-//        Properties props = new Properties();
-//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-//        props.put(ProducerConfig.ACKS_CONFIG, "all");
-//        props.put(ProducerConfig.RETRIES_CONFIG, 3);
-//        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-////        props.put(ProducerConfig.CLIENT_DNS_LOOKUP_CONFIG, "use_all_dns_ips");  // For multi-IP resolution
-////        props.put(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, 1000);            // Reconnect delay
-////        props.put(ProducerConfig.RECONNECT_BACKOFF_MAX_MS_CONFIG, 10000);       // Max reconnect delay
-////        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
-//        kafkaProducer = new KafkaProducer<>(props);
-//    }
+    static {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "lambda-producer");
+
+        kafkaProducer = new KafkaProducer<>(props);
+    }
 
     @Override
     public String handleRequest(DynamodbEvent dynamodbEvent, Context context) {
-//        try (Socket socket = new Socket("13.203.157.163", 9092)) {
-//            kafkaProducer.partitionsFor(TOPIC);
-//            kafkaTemplate.partitionsFor(TOPIC);
-//            logger.info("Successfully connected to Kafka broker.");
-//        } catch (IOException e) {
-//            logger.error("Failed to connect to Kafka broker.", e);
-//        }
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress("13.203.157.163", 9092), 2000);
+            logger.info("Kafka connection successful!");
+        } catch (Exception e) {
+            logger.error("Kafka connection failed!", e);
+        }
 
         dynamodbEvent.getRecords().forEach(record -> {
             String eventName = record.getEventName();  // INSERT, MODIFY
@@ -62,8 +60,8 @@ public class DynamoDBStreamHandler implements RequestHandler<DynamodbEvent, Stri
                     logger.info("New Record Image: {}", record.getDynamodb().getNewImage());
                     try {
                         String jsonData = objectMapper.writeValueAsString(record.getDynamodb().getNewImage());
-//                        kafkaProducer.send(new ProducerRecord<>(TOPIC, jsonData));
-//                        kafkaTemplate.send(TOPIC, jsonData);
+                        logger.info("Before publishing event to Kafka: {}", jsonData);
+                        kafkaProducer.send(new ProducerRecord<>(TOPIC, jsonData));
                         logger.info("Published event to Kafka: {}", jsonData);
                     } catch (Exception e) {
                         logger.error("Error sending data to Kafka", e);
